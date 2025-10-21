89cef7f8f3157230b2090dfd004a0b393fb7642e
Paul Vanden Broeck <PVB96P@gmail.com>
2025-10-21 09:55:08 -0400

    adding movableTestEntity

---
+++ Enceladus.Core.Tests/Helpers/MovableTestEntity.cs
@@ -0,0 +1,29 @@
+﻿using Enceladus.Core.Entities;
+using Enceladus.Core.Physics.Hitboxes;
+using Enceladus.Core.Physics.Motion;
+using System.Numerics;
+
+namespace Enceladus.Core.Tests.Helpers
+{
+    public class MovableTestEntity : Entity, IMovable
+    {
+        public MovableTestEntity()
+        {
+            Guid = Guid.NewGuid();
+            Hitbox = new RectHitbox(new(1, 1)); // Default hitbox
+        }
+
+        public override IHitbox Hitbox { get; set; }
+        public float Mass { get; set; }
+        public Vector2 Velocity { get; set; }
+        public float Drag { get; set; }
+        public float AngularVelocity { get; set; }
+        public float AngularDrag { get; set; }
+
+        public void Accelerate(Vector2 force, float deltaTime) { }
+
+        public void ApplyTorque(float torque, float deltaTime) { }
+
+        public override void Update(float deltaTime) { }
+    }
+}
