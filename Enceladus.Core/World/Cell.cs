ea697c23c2ea60f24a08ab9b96ee79a30af50dc9
Paul Vanden Broeck <PVB96P@gmail.com>
2025-10-25 23:43:57 -0400

    making cells x and y only settable at init so they cannot be accidentally changed

--- Enceladus.Core/World/Cell.cs
+++ Enceladus.Core/World/Cell.cs
@@ -10,9 +10,8 @@ namespace Enceladus.Core.World
         {
         }
 
-        //todo: maybe refactor cell to just have vector position and not separate x,y?
-        public int X { get; set; } = 0;
-        public int Y { get; set; } = 0;
+        public int X { get; init; } = 0;
+        public int Y { get; init; } = 0;
         public required CellType CellType { get; set; }
         public int Health { get; set; }
         public readonly bool HasCollision => CellType.HasCollision;
